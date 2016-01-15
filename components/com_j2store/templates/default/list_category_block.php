c427b62245e70475873bd390cf47b64f01f38ee7
Lewis Jang <Lewis.AEdes@gmail.com>
2016-01-15 16:45:56 +0800

    it's better for list to create child block

--- components/com_j2store/templates/default/list_category_block.php
+++ components/com_j2store/templates/default/list_category_block.php
@@ -20,14 +20,6 @@
             // 給予版型這個人資料狀態
             $this->filters['nowStateCat'] = $cat;
             echo $this->loadTemplate('category_list');
-
-            // 如果有女建立下個家庭
-            if (! empty($cat->childCat))
-            {
-                // give next state
-                $this->filters['nowStateCats'] = $cat->childCat;
-                echo $this->loadTemplate('category_block');
-            }
         }
         ?>
     </ul>
--- components/com_j2store/templates/default/list_category_list.php
+++ components/com_j2store/templates/default/list_category_list.php
@@ -2,5 +2,16 @@
 <?php if (! empty($cat) and is_object($cat)): ?>
     <li>
         <?php echo $cat->title; ?>
+        <?php
+
+        // 如果有女建立下個家庭
+        if (! empty($cat->childCat))
+        {
+            // give next state
+            $this->filters['nowStateCats'] = $cat->childCat;
+            echo $this->loadTemplate('category_block');
+        }
+
+        ?>
     </li>
 <?php endif; ?>
